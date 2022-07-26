3e1afd809547ba61c0caf8f2fd19081f7f56fbce
Brian Alin Monoranu <71988258+BrianAlinMonoranu@users.noreply.github.com>
2022-07-26 21:41:21 +0100

    Update App.js
    Cleaned Code

--- access/src/App.js
+++ access/src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import Sidebar from './Sidebar';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        div className = 'app__body' > { /* SideBar*/ } <
-        Sidebar / > { /* Chat */ } <
-        /div> <
-        /div>
-    );
+  return (
+    <div className="App">
+      <div className='app__body'>
+         {/* SideBar*/}
+         <Sidebar />
+         {/* Chat */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
